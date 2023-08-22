952f6f8d878168557742da1652efc78f0d0b4de5
Erico <ericoam.mussulin5@gmail.com>
2023-08-22 20:10:16 -0300

    definidas as rotas do sistema

--- my-todo-list/src/routes/AppRoutes.jsx
+++ my-todo-list/src/routes/AppRoutes.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from "../pages/Home/HomePage";
+import { ToDo } from "../pages/ToDo/ToDo";
+import { ErrorPage } from "../pages/error/ErrorPage";
 
-export default function AppRoutes(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route/>
-            </Routes>
-        </BrowserRouter>
-    )
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="todo" element={<ToDo/>}/>
+        <Route path="/todo/:id" element={<ToDo/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
+      </Routes>
+    </Router>
+  );
 }
