00caf2019506c472d1614e80d49fd0edad29859b
Erico <ericoam.mussulin5@gmail.com>
2023-08-22 20:16:57 -0300

    definindo rotas que utilizarão layoults

--- my-todo-list/src/routes/AppRoutes.jsx
+++ my-todo-list/src/routes/AppRoutes.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "../pages/Home/HomePage";
 import { ToDo } from "../pages/ToDo/ToDo";
 import { ErrorPage } from "../pages/error/ErrorPage";
+import { AppLayout } from "../layouts/AppLayout";
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="todo" element={<ToDo/>}/>
-        <Route path="/todo/:id" element={<ToDo/>}/>
-        <Route path="*" element={<ErrorPage/>}/>
+        <Route path="/" element={<AppLayout/>}>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="todo" element={<ToDo />} />
+          <Route path="/todo/:id" element={<ToDo />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
       </Routes>
     </Router>
   );
