b875b9f89e8fdac6e3fc630686542c26a27dbc21
Erico <ericoam.mussulin5@gmail.com>
2023-08-25 18:25:41 -0300

    API implementada na todoPage

--- my-todo-list/src/pages/Home/HomePage.jsx
+++ my-todo-list/src/pages/Home/HomePage.jsx
@@ -16,7 +16,6 @@ export const HomePage = () => {
 
   useEffect ( () =>{
     getTasks();
-    console.log(tasks)
   },[])
 
   return (
--- my-todo-list/src/pages/ToDo/ToDo.jsx
+++ my-todo-list/src/pages/ToDo/ToDo.jsx
@@ -1,22 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { FormComponent } from '../../components/form/FormComponent'
-import { useParams } from 'react-router-dom'
-import { MockUtils } from '../../utils/mock';
-
+import React, { useEffect, useState } from "react";
+import { FormComponent } from "../../components/form/FormComponent";
+import { useParams } from "react-router-dom";
+import { MockUtils } from "../../utils/mock";
+import { ApiService } from "../../services/ApiService";
 
 export const ToDo = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [todo, setTodo] = useState();
+  const taskService = new ApiService("tasks");
 
-  useEffect ( () =>{
-      if (id){
-        setTodo(MockUtils.todos.find(item=>item.id === Number(id)));
-      }
-  },[])
+  const ObterTask = async () => {
+    await taskService.ReadId(id).then((response) => {
+      setTodo(response);
+    });
+  };
+
+  useEffect(() => {
+    if (id) {
+      ObterTask();
+    }
+  }, []);
 
   return (
-    <div className='TodoFormContainer'>
-      <FormComponent todo={todo}/>
+    <div className="TodoFormContainer">
+      <FormComponent todo={todo} />
     </div>
-  )
-}
+  );
+};
