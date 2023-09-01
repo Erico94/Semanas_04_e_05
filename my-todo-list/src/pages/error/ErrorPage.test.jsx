2a8d1cf714d4168607c856dc72e95826d984177a
Erico <ericoam.mussulin5@gmail.com>
2023-09-01 10:15:59 -0300

    Implemntado testes unitários na errorPage

--- my-todo-list/src/pages/error/ErrorPage.test.jsx
+++ my-todo-list/src/pages/error/ErrorPage.test.jsx
@@ -1,19 +1,39 @@
 import '@testing-library/jest-dom';
 import {BrowserRouter} from 'react-router-dom'
 import {ErrorPage} from './ErrorPage'
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { beforeEach, describe, expect } from 'vitest';
-import { Button } from '../../components/button/ButtonComponent.style';
-import ButtonComponent from '../../components/button/ButtonComponent';
+import { vi } from 'vitest';
 
-describe('Teste da ErrorPage', () =>{
+const mockNavigateTo = vi.fn();
+
+vi.mock('react-router-dom', async()=>{
+    const actual = await vi.importActual('react-router-dom');
+
+    return{
+        ...actual,
+        useNavigate: () => mockNavigateTo,
+    }
+})
+
+
+
+
+
+describe('Test suit ErrorPage', () =>{
     beforeEach(()=>{
         render(<ErrorPage/>,  { wrapper: BrowserRouter })
     })
-    it('Verifica se contém Opss...', () =>{
+
+    it('Should be able to render ErrorPage component',()=>{
+        expect(screen.getByTestId('error-page-component')).toBeInTheDocument();
+    })
+    it('Should be able to render the text "Opss"', () =>{
         expect(screen.getByText(/Opss/i)).toBeInTheDocument();
     }) 
-    it('Verifica se o button foi renderizado', ()=>{
-        expect(screen.getByRole('button')).toBeInTheDocument();
-    })
+
+    it('Should be able to redirect user to home page',()=>{
+        fireEvent.click(screen.getByText(/Voltar para pagina inicial/));
+        expect(mockNavigateTo).toHaveBeenCalledWith('/');
+    }) 
 })
