53d9169ac119bbe72a87fdca8aca26f95ddd041f
Erico <ericoam.mussulin5@gmail.com>
2023-09-01 09:04:07 -0300

    Implementados os testes do buttonComponent

--- my-todo-list/src/pages/error/ErrorPage.jsx
+++ my-todo-list/src/pages/error/ErrorPage.jsx
@@ -1,22 +1,26 @@
-import { useNavigate } from "react-router-dom"
-import { ButtonComponent } from "../../components/button/ButtonComponent"
+import { useNavigate } from "react-router-dom";
+import { ButtonComponent } from "../../components/button/ButtonComponent";
 
-import * as Styled from './ErrorPage.style'
+import * as Styled from "./ErrorPage.style";
 
 export const ErrorPage = () => {
   const navigate = useNavigate();
-  
+
   const navigateTo = (route) => {
     navigate(route);
-  }
+  };
 
   return (
-    <Styled.ErrorContainer>
+    <Styled.ErrorContainer data-testid="error-page-component">
       <h3>Opss! Página não encontrada...</h3>
       <p>Não Conseguimos encontrar a página solicitada!</p>
-      <ButtonComponent onClick={() => { navigateTo('/') }}>
-        Votar para pagina inicial
+      <ButtonComponent
+        onClick={() => {
+          navigateTo("/");
+        }}
+      >
+        Voltar para página inicial
       </ButtonComponent>
     </Styled.ErrorContainer>
-  )
-}
+  );
+};
